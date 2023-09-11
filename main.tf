3d4dd18103560a4bddaef9327108a363747ae32c
Margarida Ferreira <67073270+mmccferreira@users.noreply.github.com>
2023-09-11 17:12:30 +0100

    Replaced all main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 data "aws_ami" "app_ami" {
   most_recent = true
 
-# this deploys a tomcat server by using this filter to find the latest version of this bitnami tomcat server
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
@@ -21,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -30,21 +29,9 @@ module "blog_vpc" {
   }
 }
 
-# it takes the image created above (aws_ami) and then it provisions an aws instance named 'web'
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -60,12 +47,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -84,14 +65,12 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-
-  vpc_id = module.blog_vpc.public_subnets[0]
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
